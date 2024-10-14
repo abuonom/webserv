c2410b4936934bff7f2e615369c6d056e571f9e7
lpiccirilli <nuclei-secchiata.0k@icloud.com>
2024-10-14 17:43:05 +0200

    post funge piu o meno

--- src/Server.cpp
+++ src/Server.cpp
@@ -132,26 +132,28 @@ void Server::handleClient(int client_fd, const ServerConfigs &serverConfigs)
 	{
 		int bytes_read = 0;
 		bytes_read = read(client_fd, buffer, BUFFER_SIZE - 1);
-		if (bytes_read <= 0)
-		{
-			close(client_fd);
-			// Rimuoviamo il client dal vector dei pollfd
-			for (std::vector<pollfd>::iterator it = _poll_fds.begin(); it != _poll_fds.end(); ++it)
-			{
-				if (it->fd == client_fd)
-				{
-					_poll_fds.erase(it);
-					break;
-				}
-			}
-			return;
-		}
 		total_read += bytes_read;
+		if (bytes_read <= 0)
+			break;
+		// if (bytes_read < 0)
+		// {
+		// 	close(client_fd);
+		// 	// Rimuoviamo il client dal vector dei pollfd
+		// 	for (std::vector<pollfd>::iterator it = _poll_fds.begin(); it != _poll_fds.end(); ++it)
+		// 	{
+		// 		if (it->fd == client_fd)
+		// 		{
+		// 			_poll_fds.erase(it);
+		// 			break;
+		// 		}
+		// 	}
+		// 	return;
+		// }
 		for (int i = 0; i < bytes_read; i++)
 			rec += buffer[i];
 	}
-		buffer[total_read] = '\0';
-		rec += '\0';
+	buffer[total_read] = '\0';
+	rec += '\0';
 	Request request(rec);
 	if (request._method == "GET")
 	{
